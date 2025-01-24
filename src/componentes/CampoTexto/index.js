65b040574240882225238c8c4f4931b6316aef19
matheussbarreto <matheus.santos.barreto@gmail.com>
2025-01-24 00:57:46 -0300

    feat: create form

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,17 @@
+import './CampoTexto.css'
+// Use arrow function
+const CampoTexto = (props) => {
+
+    const aoDigitar = (evento) => {
+        props.aoAlterar(evento.target.value);
+    }
+    
+    return (
+        <div className='campo-texto'>
+            <label>{props.label}</label>
+            <input value={props.valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default CampoTexto;
