2ee07c4f04e229e6434ab1fb1d64a4979fd2abaf
matheussbarreto <matheus.santos.barreto@gmail.com>
2025-01-24 13:20:41 -0300

    feat: add footer on page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 function App() {
 
@@ -46,7 +47,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador);
     setColaboradores([...colaboradores, colaborador]);
   }
 
@@ -62,7 +62,7 @@ function App() {
         corSecundaria={time.corSecundaria} 
         colaboradores={colaboradores.filter( colaborador => colaborador.time === time.nome)}
       />)}
-    
+      <Rodape />
     </div>
   );
 }
